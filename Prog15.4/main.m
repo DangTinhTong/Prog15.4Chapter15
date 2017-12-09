b80bee05ae7a7ea0a86bd850b546e86c29c638aa
admin <dangtinhtong@gmail.com>
2017-12-09 23:27:11 +0700

    First commit

--- Prog15.4/main.m
+++ Prog15.4/main.m
@@ -6,12 +6,50 @@
 //  Copyright © 2017 admin. All rights reserved.
 //
 
+// Basic string operations
+
+
 #import <Foundation/Foundation.h>
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
         // insert code here...
-        NSLog(@"Hello, World!");
+        //NSLog(@"Hello, World!");
+        
+        NSString *myString1 = @"This is string A";
+        NSString *myString2 = @"This is string B";
+        NSString *res;
+        NSRange subRange;
+        
+        
+        // Extract first 3 chars from string
+        res = [myString1 substringFromIndex:3];
+        NSLog(@"First 3 chars of myString1: %@",res);
+        
+        // Extract chars to end of string starting at index 5
+        res =[myString1 substringFromIndex:5];
+        NSLog(@"Chars from index 5 of myString1: %@",res);
+        
+        //  Extract chars from index 8 thourgh 13 (6 chars)
+        res=[[myString1 substringFromIndex:8] substringFromIndex:6];
+        NSLog(@"Chars from index 8 through 13: %@:",res);
+        
+        
+        // An easier way to do the same thing
+        res = [myString1 substringWithRange:NSMakeRange(8, 6)];
+        NSLog(@"Chars from index 8 through 13: %@: ",res);
+        
+        // locate one string inside another
+        
+        subRange = [myString1 rangeOfString:@"String A"];
+        NSLog(@"String is at index %lu, length is %lu", subRange.location,subRange.length);
+        subRange = [myString1 rangeOfString:@"String B"];
+        if(subRange.location== NSNotFound)
+            NSLog(@"String not found");
+        else
+            NSLog(@"String is at index %lu, length %lu", subRange.location, subRange.length);
+        
+        
     }
     return 0;
 }
